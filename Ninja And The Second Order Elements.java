2093bf4fd6335dcad1448cbea35c16afce6385be
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-07-09 18:22:34 +0530

    Create Ninja And The Second Order Elements.java

---
+++ Ninja And The Second Order Elements.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public static int[] getSecondOrderElements(int n, int []arr) {
+        // Write your code here.
+
+        int ans[]=new int[2];
+
+       
+
+        int small = Integer.MAX_VALUE;
+	int second_small = Integer.MAX_VALUE;
+	int large = Integer.MIN_VALUE;
+	int second_large = Integer.MIN_VALUE;
+	int i;
+	for (i = 0;i < n;i++)
+	{
+		small = Math.min(small,arr[i]);
+		large = Math.max(large,arr[i]);
+	}
+	for (i = 0;i < n;i++)
+	{
+		if (arr[i] < second_small && arr[i] != small)
+		{
+			second_small = arr[i];
+		}
+		if (arr[i] > second_large && arr[i] != large)
+		{
+			second_large = arr[i];
+		}
+	}
+        ans[0]=second_large;
+        ans[1]=second_small;
+        return ans;
+    }
+}
