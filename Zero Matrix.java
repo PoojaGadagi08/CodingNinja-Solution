713cd0851873180501cf3c0f4b6a70778fb227fc
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-08-01 13:57:42 +0530

    Create Zero Matrix.java

---
+++ Zero Matrix.java
@@ -0,0 +1,34 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
+    	// Write your code here.
+         int[] row = new int[n]; // row array
+        int[] col = new int[m]; // col array
+
+        // Traverse the matrix:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    // mark ith index of row wih 1:
+                    row[i] = 1;
+
+                    // mark jth index of col wih 1:
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Finally, mark all (i, j) as 0
+        // if row[i] or col[j] is marked with 1.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
